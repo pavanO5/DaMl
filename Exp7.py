0c8994bd62df665f9894353796cd641e134c8f1b
pavan <pavanpatro7@gmail.com>
2025-08-07 14:34:24 +0530

    initial commit

--- Exp7.py
+++ Exp7.py
@@ -4,21 +4,19 @@ iris = pd.read_csv("C:/Users/pavan/Downloads/IRIS (1).csv")
 print(iris)
 import numpy as np
 
+#Multiple Linear Regression
 x = iris[['SepalLengthCm', 'SepalWidthCm']]
 y = iris.PetalLengthCm
-
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
-
 from sklearn.linear_model import LinearRegression
 LR = LinearRegression()
 training = LR.fit(x_train, y_train)
 y_pred = LR.predict(x_test)
 print(y_pred)
-
+#Performance Metrics
 import numpy as np
 from sklearn import metrics
-
 MAE = metrics.mean_absolute_error(y_test, y_pred)
 print("Mean Absoute Error: ", MAE)
 MSE = metrics.mean_squared_error(y_test, y_pred)
@@ -26,23 +24,19 @@ print("Mean Squeared Error: ", MSE)
 RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
 print("RMSE: ", RMSE)
 
-
+#Simple Linear Regression
 x = iris[['SepalLengthCm']]
 y = iris.PetalLengthCm
-
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
-
 from sklearn.linear_model import LinearRegression
 LR = LinearRegression()
-
 training = LR.fit(x_train, y_train)
 y_pred = LR.predict(x_test)
 print(y_pred, "\n")
-
+#Performance Metrics
 import numpy as np
 from sklearn import metrics
-
 MAE = metrics.mean_absolute_error(y_test, y_pred)
 print("Mean Absoute Error: ", MAE)
 MSE = metrics.mean_squared_error(y_test, y_pred)
